af31ceedd6382559710229982df462744bc8d6fe
Vincent Jacques <vincent@vincent-jacques.net>
2018-02-28 11:52:43 +0100

    Keep track of current and start point in move_to and line_to

--- src/CairoMock/CairoMock.ml
+++ src/CairoMock/CairoMock.ml
@@ -1,15 +1,29 @@
-(* open StdLabels *)
+open StdLabels
 
 module Backend = Backend
 
 include Backend
 
+module State = struct
+  type t = {
+    transformation: Matrix.t;
+  }
+end
+
 type context = {
   mutable calls: string list;
+  mutable states: State.t list;
+  points: Points.t;
 }
 
 let create () = {
   calls = [];
+  states = [
+    {
+      transformation = Matrix.init_identity ();
+    };
+  ];
+  points = Points.create ();
 }
 
 let call context =
@@ -17,12 +31,21 @@ let call context =
     context.calls <- call::context.calls
   )
 
-let calls {calls} =
+let calls {calls; _} =
   calls
+
+let state {states; _} =
+  List.hd states
 
 
 let move_to context ~x ~y =
-  call context "move_to ~x:%.2f ~y:%.2f" x y
+  call context "move_to ~x:%.2f ~y:%.2f" x y;
+  let transformation = (state context).transformation in
+  Points.set_start context.points ~transformation ~x ~y;
+  Points.set_current_from_start context.points
 
 let line_to context ~x ~y =
-  call context "line_to ~x:%.2f ~y:%.2f" x y
+  call context "line_to ~x:%.2f ~y:%.2f" x y;
+  let transformation = (state context).transformation in
+  Points.set_start_if_none context.points ~transformation ~x ~y;
+  Points.set_current context.points ~transformation ~x ~y
